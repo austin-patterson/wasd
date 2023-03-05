ccd7a465efa7fc66e60b6e75bdca4102b1099082
Austin Patterson <pattersonau@msoe.edu>
2023-03-05 14:31:17 -0600

    setup light theme by default

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,23 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import Tailwind from './components/Tailwind.vue';
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-
-
-  <h1 class="text-3xl font-bold underline">
-    TailwindCSS is on!
-  </h1>
-
-  <HelloWorld msg="Vite + Vue" />
+  <!--   
+    <div>
+      <a href="https://vitejs.dev" target="_blank">
+        <img src="/vite.svg" class="logo" alt="Vite logo" />
+      </a>
+      <a href="https://vuejs.org/" target="_blank">
+        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      </a>
+    </div>
+  -->
+  
+  <Tailwind />
+  
+  <!-- <HelloWorld msg="Vite + Vue" /> -->
 </template>
 
 <style scoped>

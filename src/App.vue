4a25f2a2fab2809f095c3cae0d7bb52abb36e8f7
austin-patterson <aus.g.patterson@gmail.com>
2023-09-12 00:43:46 -0500

    sketch

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import Sketch from './components/Sketch.vue';
 </script>
 
 <template>
-  <div>
+
+  <Sketch />
+
+
+  <!-- <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
@@ -11,7 +16,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld msg="Vite + Vue" /> -->
 </template>
 
 <style scoped>
---
+++ src/components/Typengine.vue
@@ -0,0 +1,35 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <h1>Typengine.Vue</h1>
+    </div>
+
+    <!-- layout -->
+    <div class="header">
+        <!-- TODO: score and timer -->
+    </div>
+
+    <div class="body">
+        <div class="container center">
+            <div class="word-box scrolling-animation-container">
+                <p class="line">
+                    <!-- if word is not focussed, unset the background color for the span so it defaults to the word-box background color -->
+                    <!-- if word is focused, give word span a rounded background color of green -->
+                    <!-- if letter has not been attempted, set the font color to black -->
+                    <!-- else if letter has been attempted -->
+                        <!-- and the letter is correct -->
+                            <!-- give letter span a font color of white -->
+                        <!-- else if letter is incorrect -->
+                            <!-- give letter span a font color of red -->
+
+                    <!-- START WITH STATIC LOREM IPSUM TEXT AND A NON-SCROLLING WINDOW -->
+                </p>
+            </div>
+        </div>
+    </div>
+    
+    <div class="footer"></div>
+</template>
